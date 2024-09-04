1fd1627d7c81d0cf983c3d07cf22a2a6f46173b7
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-09-05 01:02:26 +0530

    asasasas

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,12 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './Pages/Redux/Store';
 import './index.css';
 import App from './App';
-import { Elements } from '@stripe/react-stripe-js';
-import { stripePromise } from './stripe';  // Make sure to create the stripePromise in a separate file as discussed earlier.
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <Elements stripe={stripePromise}>
+      
         <App />
         <ToastContainer
           position='top-right'
@@ -26,7 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           pauseOnHover
           theme='colored'
         />
-      </Elements>
+     
     </PersistGate>
   </Provider>
 );
