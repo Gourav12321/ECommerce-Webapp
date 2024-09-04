b2a244e77a54fc0640a66a166589ca79648c3665
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-09-05 02:34:19 +0530

    asdadas

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 3000, // Increase the chunk size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Example of manual chunking
+        },
+      },
+    },
+  },
+});
