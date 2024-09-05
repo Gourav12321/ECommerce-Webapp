cd1a7fc5a5064e49fa0249d1f754704634b89579
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-09-05 12:29:04 +0530

    sadasdasdsa

--- backend/index.js
+++ backend/index.js
@@ -11,38 +11,46 @@ const orderRoutes = require('./routes/Orders.route');
 const AdminDashboard = require('./routes/AdminDashboard.route');
 const wishlist = require('./routes/wishlist.routes');
 const path = require('path');
+
+// Load environment variables
 dotenv.config();
 
 const app = express();
 
-
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-const __dirname = path.join();
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Database connected");
-}).catch((error) => {
-  console.log(error);
-});
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+// Serve static files from the 'client/dist' directory
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
+// Handle all other routes and send the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// API routes
 app.use('/api/user', UserAuthRoute);
 app.use('/api', ProductRoute);
 app.use('/api', CategoryRoute);
 app.use('/api/cart', OrderRoute);
 app.use('/api/order', orderRoutes);
 app.use('/api', AdminDashboard);
-app.use('/api',wishlist);
+app.use('/api', wishlist);
+
+// Serve static files from the 'receipts' directory
 app.use('/receipts', express.static(path.join(__dirname, 'receipts')));
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
